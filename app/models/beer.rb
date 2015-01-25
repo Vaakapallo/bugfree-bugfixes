de5e2cad88753967646910cc37a480221183f1ee
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-01-25 15:47:58 +0200

    Week 2, Ex. 1-4 stuff with ratings

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,13 @@
 class Beer < ActiveRecord::Base
-	belongs_to :brewery
+  	belongs_to :brewery
+  	has_many :ratings
+
+  	def average_rating
+  		sum = 0
+  		ratings.each do |rating|
+  			sum += rating.score
+  		end
+  		sum /= ratings.count
+  		return sum
+  	end
 end
